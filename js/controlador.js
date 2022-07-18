4d4dd7751cd639390ad9ff4d9c68ad55b43c9a67
OnMyMind-28 <taliasalvador@yahoo.com>
2022-07-17 18:28:51 -0600

    avance10

--- js/controlador.js
+++ js/controlador.js
@@ -4,7 +4,7 @@ var empresa = [
         nombreEmpresa: 'Samsung',
         urlImagen: 'img/Tecnologia/Empresas/1.emp.png',
         categoria: 'Tecnología',
-        calificacion: 3
+        calificacion: 4
     },
     {//JSON 2
         nombreEmpresa: 'Sony',
@@ -16,13 +16,13 @@ var empresa = [
         nombreEmpresa: 'HP',
         urlImagen: 'img/Tecnologia/Empresas/3.emp.png',
         categoria: 'Tecnología',
-        calificacion: 2
+        calificacion: 5
     },
     {//JSON 4
         nombreEmpresa: 'Apple',
         urlImagen: 'img/Tecnologia/Empresas/4.emp.png',
         categoria: 'Tecnología',
-        calificacion: 3
+        calificacion: 5
     }
    
 ];
@@ -42,17 +42,19 @@ function generarempresa(){
         document.getElementById('empresa').innerHTML += 
         ` <div class=" mt-5 "> 
                <div class="card">
-               <div class="Padre grid mx-3">
-               <div class="Hijo " style="background-color: white; height: 300px; width: 49%;">
-                <img src="${emp.urlImagen}" class="mt-5 card-img-left mx-5">
+               <div class="Padre grid ">
+               <div class="Hijo " style="background-color: white; height: 100px; width: 600px;">
+                <img src="${emp.urlImagen}" class="imgCard card-img-left my-auto">
                 </div>
                 <div class="Hijo card-body mt-5" >
-                    <h3 class="card-title" onclick="Irempresa()">${emp.nombreEmpresa}</h3>
-                    <p class="card-text"> Categoría: ${emp.categoria}</p>
+                    <h3 class="card-title " onclick="Irempresa()">${emp.nombreEmpresa}</h3>
+                    <p>Acepta pago online</p>
+                    <p class="card-text "> Categoría: ${emp.categoria}</p>
                      <div>
                        ${estrellas}
                        
                     </div>
+
                </div>
                </div>
             </div>`;
